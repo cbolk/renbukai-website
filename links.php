ce27329c78bbf14e6dd4c30ef1dc0e72c33c38b0
Cristiana Bolchini <cristiana.bolchini@gmail.com>
2016-01-24 16:37:16 +0100

    link page: style and spacing

--- links.php
+++ links.php
@@ -33,38 +33,38 @@
             <?php include('./utils/head_banner.php'); ?>
         </div>
         <div class="row">
-            <h3>Aikido</h3>
+              <h3>Aikido</h3>
         </div>
         <div class="col-xs-12">
             <h4>Italia</h4>
             <ul>
-            <li><span class="name">Aikikai Italia</span><span class="address"><a href="http://www.aikikai.it" target=_blank>www.aikikai.it</a></span></li>
-            <li><span class="name">Aikikai Milano - Dojo Fujimoto</span><span class="address"><a href="http://www.aikidofujimoto.it" target=_blank>www.aikidofujimoto.it</a></span></li>
+            <li><span class="name">Aikikai Italia</span> <span class="address"><a href="http://www.aikikai.it" target=_blank>www.aikikai.it</a></span></li>
+            <li><span class="name">Aikikai Milano - Dojo Fujimoto</span> <span class="address"><a href="http://www.aikidofujimoto.it" target=_blank>www.aikidofujimoto.it</a></span></li>
             </ul>
             <h4>Giappone</h4>
             <ul>
-            <li><span class="name">Aikikai Honbu Dojo</span><span class="address"><a href="http://www.aikikai.or.jp" target=_blank>www.aikikai.or.jp</a></span></li>
-            <li><span class="name">Tada Hiroshi HP</span><span class="address"><a href="http://www.asahi-net.or.jp/~yp7h-td/index_it.html" target=_blank>www.asahi-net.or.jp/~yp7h-td/index_it.html</a></span></li>
-            <li><span class="name">Tada Juku Gessoji Dojo</span><span class="address"><a href="http://www.h5.dion.ne.jp/~aiki-dou/" target=_blank>www.h5.dion.ne.jp/~aiki-dou/</a></span></li>
+            <li><span class="name">Aikikai Honbu Dojo</span> <span class="address"><a href="http://www.aikikai.or.jp" target=_blank>www.aikikai.or.jp</a></span></li>
+            <li><span class="name">Tada Hiroshi HP</span> <span class="address"><a href="http://www.asahi-net.or.jp/~yp7h-td/index_it.html" target=_blank>www.asahi-net.or.jp/~yp7h-td/index_it.html</a></span></li>
+            <li><span class="name">Tada Juku Gessoji Dojo</span> <span class="address"><a href="http://www.h5.dion.ne.jp/~aiki-dou/" target=_blank>www.h5.dion.ne.jp/~aiki-dou/</a></span></li>
             </ul>
         </div>
         <div class="row">
-            <h3>Onoha itto ryu</h3>
+              <h3>Onoha itto ryu</h3>
         </div>
         <div class="col-xs-12">
             <h4>Giappone</h4>
             <ul>
-            <li><span class="name">Reigakudo</span><span class="address"><a href="http://onohaittoryu.3.pro.tok2.com/main.html" target=_blank>onohaittoryu.3.pro.tok2.com/main.html</a></span></li>
+            <li><span class="name">Reigakudo</span> <span class="address"><a href="http://onohaittoryu.3.pro.tok2.com/main.html" target=_blank>onohaittoryu.3.pro.tok2.com/main.html</a></span></li>
             </ul>
             <h4>Italia</h4>
             <ul>
-            <li><span class="name">Onoha Itto Ryu Italia</span><span class="address"><a href="http://www.onohaittoryuitalia.org/" target=_blank>www.onohaittoryuitalia.org/</a></span></li>
+            <li><span class="name">Onoha Itto Ryu Italia</span> <span class="address"><a href="http://www.onohaittoryuitalia.org/" target=_blank>www.onohaittoryuitalia.org/</a></span></li>
             </ul>
             <h4>Germania</h4>
             <ul>
-            <li><span class="name">Onoha Ittoryu Keiko Deutschland</span><span class="address"><a href="http://www.onoha-ittoryu.de/" target=_blank>www.onoha-ittoryu.de</a></span></li>
-            </ul>
-		</div>
+            <li><span class="name">Onoha Ittoryu Keiko Deutschland</span> <span class="address"><a href="http://www.onoha-ittoryu.de/" target=_blank>www.onoha-ittoryu.de</a></span></li>
+            </ul>            
+        </div>    
         <div class="clearfix mobile">&nbsp;</div>
             <footer class="footer mobile">
                 <div class="container acenter">
