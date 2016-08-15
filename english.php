d7b458e0ffa5381e310335e280e96dec49d72065
Cristiana Bolchini <cristiana.bolchini@gmail.com>
2016-08-15 20:13:27 +0200

    English page

--- english.php
+++ english.php
@@ -38,10 +38,10 @@
 	        <h3>Welcome to Aikido Renbukai Pesaro / Rimini</h3>
 		</div>            
         <div class="row">
-        	<div class="col-xs-12 col-sm-6 col-lg-6 ">
+        	<div class="col-xs-12 col-sm-4 col-lg-4">
               	<div class="acenter pad"><img src="./images/rf03.jpg"/></div>
         	</div>
-        	<div class="col-xs-12 col-sm-6 col-lg-6">
+        	<div class="col-xs-12 col-sm-8 col-lg-8">
                 <p></p>
         		<p>Aikido Renbukai is an Aikido school with two dojos, one in Pesaro and the other in Rimini, affiliated to <a href='http://www.aikikai.it' target=_blank>Aikikai d'Italia</a>.</p>
                 <p>Pesaro dojo hosts weekly practise for one of the Italian Onoha Itto Ryu Practise Groups (more information available <a target=_blank href='http://www.onohaittoryuitalia.org/Onoha_Itto_Ryu_Italia/Introduction.html'>here</a>).</p>
@@ -51,17 +51,44 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-xs-12">    
-                 <h4 name="schedule">How to contact / find us</h4>
+            <div class="col-xs-12 text-center">    
+                 <h2 name="schedule" class="text-primary">How to contact / find us</h2>
+            </div>
+        </div>
+      <div class="row">
+        <p><br/>&nbsp;&nbsp;&nbsp;You can reach us in one of these ways:
+            <ul>
+              <li>come and see us in our dojo, in Pesaro or Rimini, during classes.
+              <li>give us a call: 340-5996157</li>
+              <li>send an email: info@aikidorenbukai.it</li>
+            </ul>
+      </div>
+      <div class="row">
+          <div class="col-md-6 col-xs-12">
+                <p><strong>Aikido Renbukai Pesaro</strong><br/>
+                <p><strong>c/o KeikoDojo</strong><br/>
+                <i class="fa fa-map-marker"></i>&nbsp;<span>Via Pietro Gai, 19 - 61100 Pesaro (PU)</span><br/><br/>
+                <div>
+                    <iframe class="nomobile" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2874.725137263933!2d12.904746115505823!3d43.902959179113566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132d194fc615be2b%3A0xfa1abbad378072fc!2sVia+Gai+Pietro%2C+19%2C+61122+Pesaro+PU!5e0!3m2!1sen!2sit!4v1451164963649" width="95%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <iframe class="mobile" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2874.725137263933!2d12.904746115505823!3d43.902959179113566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132d194fc615be2b%3A0xfa1abbad378072fc!2sVia+Gai+Pietro%2C+19%2C+61122+Pesaro+PU!5e0!3m2!1sen!2sit!4v1451164963649" width="95%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+
+          </div>
+            <div class="col-md-6 col-xs-12">
+                <p class="mobile">&nbsp;<br/>&nbsp;</p>
+                <p><strong>Aikido Renbukai Rimini</strong><br/>
+                <p><strong>c/o NikoNiko</strong><br/>
+                <i class="fa fa-map-marker"></i>&nbsp;<span>Via Madonna della scala, 53/a - 47800 Rimini (RN)</span><br/>
+                (enter in via Tancredi Saletta, lefthand side of the building)<br/>
+                <div>
+                    <iframe class="nomobile" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2866.770956148851!2d12.559866714904427!3d44.067438033564414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132cc397327cec13%3A0x98578c694d5818ff!2sVia+Madonna+della+Scala%2C+53%2C+47921+Rimini+RN!5e0!3m2!1sen!2sit!4v1451165839968" width="95%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <iframe class="mobile" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2866.770956148851!2d12.559866714904427!3d44.067438033564414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132cc397327cec13%3A0x98578c694d5818ff!2sVia+Madonna+della+Scala%2C+53%2C+47921+Rimini+RN!5e0!3m2!1sen!2sit!4v1451165839968" width="95%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
             </div>
         </div>
         <div class="row">
-
-
-        </div>
-        <div class="row">
-            <div class="col-xs-12">    
-                 <h4 name="schedule">Class schedule</h4>
+            <div class="col-xs-12 text-center">    
+                 <h2 name="schedule" class="text-primary">Class schedule</h2>
             </div>
         </div>
         <div class="row">
@@ -235,13 +262,33 @@
                 </ul>
             </div>
         </div>      
-        <div class="clearfix mobile">&nbsp;</div>
-        <footer class="footer mobile">
-            <div class="container acenter">
-                <p class="text-muted"><img src="./assets/footer.png" alt=""></p>
-            </div>
-        </footer>
-    </div><!-- container -->
+      <div class="row">
+        <div class="col-xs-12">
+          <h2 class="text-center text-primary">Upcoming event</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-md-4 text-right">
+          <a href='./stages/20160819.pdf' title='scarica la locandina seminario'><img src="./stages/20160819.thumb.png" /></a>
+        </div>
+        <div class="col-xs-12 col-md-8">
+          <div class="maintitle"><h3>Aug 19-21 2016 @ Borgo Pace</h3></div>
+          <div class="caption">
+              <p>Weapons seminar - R. Foglietta</p>
+              <p>Schedule: Fri. 18-20<br/>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sat. 07-08&nbsp;&nbsp;|&nbsp;&nbsp;09-11&nbsp;&nbsp;|&nbsp;&nbsp;17-19<br/>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sun. 07-08&nbsp;|&nbsp;&nbsp;09-11&nbsp;&nbsp;lunch</p>
+
+              <p>Bring your weapons: jo, bokken e tanto</p>
+          </div>
+        </div>
+      </div>
+      <div class="clearfix">&nbsp;</div>
+      <footer class="footer">
+        <div class="container acenter">
+            <p class="text-muted"><img src="./assets/footer.png" alt=""></p>
+        </div>
+      </footer>    </div><!-- container -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script>    
 
