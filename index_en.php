871dfbe5347e17a45f54203428f190258fd02d54
Cristiana Bolchini <cristiana.bolchini@gmail.com>
2016-08-18 17:51:24 +0200

    English temporary page

--- index_en.php
+++ index_en.php
@@ -31,26 +31,26 @@
 
       <!--div class="row"-->
         <p class="nomobile">&nbsp;<br/>&nbsp;<br/>&nbsp;</p>
-        <h2 class="text-center text-primary">Sito in fase di aggiornamento&nbsp;&nbsp;<a class='noborder' href="./index_en.php" title="information in English"><img class='noborder' alt='English flag' src='./assets/english32.png' width="24" /></a></h2>
-        <p>&nbsp;<br/>Benvenuti nel sito dell'Associazione Renbukai, scuola di Aikido, che a Pesaro ospita presso il Keikodojo uno dei sei Gruppi di Pratica di Onoha Itto Ryu in Italia.<br/>
+        <h2 class="text-center text-primary">New website under construction&nbsp;&nbsp;<a class='noborder' href="./index.php" title="information in Italian"><img class='noborder' alt='Italian flag' src='./assets/italian32.png' width="24" /></a></h2>
+        <p>&nbsp;<br/>Associazione Renbukai is an Aikido school with two dojos, one in Pesaro and the other one in Rimini, affiliated to <a href='http://www.aikikai.it' target=_blank>Aikikai d'Italia</a>.<br/>
 
-Il responsabile didattico &egrave; il Maestro Roberto Foglietta, VI dan Aikikai d'Italia e Aikikai Tokyo.<br/>
+        We have both general and youngster classes every <a href='#schedule'>day</a> at our dojos, and it is possible to enroll at any time during the year.<br/>
 
-L'Associazione Renbukai &egrave; affiliata all'Associazione di Cultura Tradizionale Giapponese Aikikai d'Italia - Ente Morale (D.P.R. luglio 1978 n. 528).</p>
+        Roberto Foglietta, VI dan Aikikai d'Italia and Aikikai Tokyo, is the chief instructor.</p>
 
       <!--/div-->
 
       <div class="row">
-        <p><br/>&nbsp;&nbsp;&nbsp;Ci potete contattare nei seguenti modi:
+        <p><br/>&nbsp;&nbsp;&nbsp;You can reach us in one of these ways:
             <ul>
-              <li>venendoci a trovare in dojo, a Pesaro e Rimini, durante l'orario di pratica (in Agosto)
+              <li>come and see us in our dojo, in Pesaro or Rimini, during classes (in August)
               <ul>
-                <li>a <strong>Pesaro</strong>: Marted&igrave;, Gioved&igrave; dalle 19:30 alle 21:00, Sabato dalle 18:30 alle 20:00</li>
-                <li>a <strong>Rimini</strong>: Mercoled&igrave; e Venerd&igrave; dalle 19:30 alle 21:00</li>
+                <li>a <strong>Pesaro</strong>: Tuesday, Thursday from 7:30PM to 9:00PM, Saturday from 6:30PM to 8:00PM</li>
+                <li>a <strong>Rimini</strong>: Wednesday and Friday from 7:30PM to 9:00PM</li>
                 </ul>
               </li>
-              <li>telefonicamente: 340-5996157</li>
-              <li>via email: info@aikidorenbukai.it</li>
+              <li>give us a call: 340-5996157</li>
+              <li>send an email: info@aikidorenbukai.it</li>
             </ul>
       </div>
       <div class="row">
@@ -69,7 +69,7 @@ L'Associazione Renbukai &egrave; affiliata all'Associazione di Cultura Tradizion
                 <p><strong>Aikido Renbukai Rimini</strong><br/>
                 <p><strong>c/o NikoNiko</strong><br/>
                 <i class="fa fa-map-marker"></i>&nbsp;<span>Via Madonna della scala, 53/a - 47800 Rimini (RN)</span><br/>
-                (ingresso da via Tancredi Saletta, lato sinistro dell'edificio)<br/>
+                (entrance from via Tancredi Saletta, lefthand-side of the building)<br/>
                 <div>
                     <iframe class="nomobile" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2866.770956148851!2d12.559866714904427!3d44.067438033564414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132cc397327cec13%3A0x98578c694d5818ff!2sVia+Madonna+della+Scala%2C+53%2C+47921+Rimini+RN!5e0!3m2!1sen!2sit!4v1451165839968" width="95%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
                     <iframe class="mobile" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2866.770956148851!2d12.559866714904427!3d44.067438033564414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132cc397327cec13%3A0x98578c694d5818ff!2sVia+Madonna+della+Scala%2C+53%2C+47921+Rimini+RN!5e0!3m2!1sen!2sit!4v1451165839968" width="95%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
@@ -78,7 +78,7 @@ L'Associazione Renbukai &egrave; affiliata all'Associazione di Cultura Tradizion
         </div>
       <div class="row">
         <div class="col-xs-12">
-          <h2 class="text-center text-primary">Prossimo appuntamento<br/></h2>
+          <h2 class="text-center text-primary">Upcoming event<br/></h2>
         </div>
       </div>
       <div class="row">
@@ -86,15 +86,14 @@ L'Associazione Renbukai &egrave; affiliata all'Associazione di Cultura Tradizion
           <a href='./stages/20160819.pdf' title='scarica la locandina seminario'><img alt="locandina stage 2016-08-19" src="./stages/20160819.thumb.png" /></a>
         </div>
         <div class="col-xs-12 col-md-8">
-          <div class="maintitle"><h3>19-21 ago 2016 @ Borgo Pace</h3></div>
+          <div class="maintitle"><h3>19-21 Aug. 2016 @ Borgo Pace</h3></div>
           <div class="caption">
-              <p>Seminario di armi diretto da R. Foglietta</p>
-              <p>Orario: Ven. 18-20<br/>
-                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sab. 07-08&nbsp;&nbsp;|&nbsp;&nbsp;09-11&nbsp;&nbsp;|&nbsp;&nbsp;17-19<br/>
-                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dom. 07-08&nbsp;|&nbsp;&nbsp;09-11&nbsp;&nbsp;pranzo</p>
+              <p>Weapons seminar - R. Foglietta</p>
+             <p>Schedule: Fri. 18-20<br/>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sat. 07-08&nbsp;&nbsp;|&nbsp;&nbsp;09-11&nbsp;&nbsp;|&nbsp;&nbsp;17-19<br/>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sun. 07-08&nbsp;|&nbsp;&nbsp;09-11&nbsp;&nbsp;lunch</p>
 
-              <p>Portare jo, bokken e tanto</p>
-              <p>Il seminario si terr&agrave; all'aperto o in una palestra priva di tatami: portare calzature adatte (ad es. scarpe da ginnastica)</p>
+              <p>Bring your weapons: jo, bokken e tanto</p>
           </div>
         </div>
       </div>
