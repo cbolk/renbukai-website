97baecff3a140573109b8845b01e673bb1d8529b
Cristiana Bolchini <cristiana.bolchini@gmail.com>
2017-10-02 08:25:22 +0200

    seminars 2017/2018

--- seminari.php
+++ seminari.php
@@ -137,7 +137,7 @@
         <div class="row">
             <div class="col-lg-12 col-xm-12">
                 <br/>
-                <p>Pagina soggetta a variazioni. Ultimo aggiornamento: <strong>27 agosto 2017</strong>.</p>
+                <p>Pagina soggetta a variazioni. Ultimo aggiornamento: <strong>30 settembre 2017</strong>.</p>
             </div>
         </div>        <!-- footer -->   
         <?php include_once("utils/footer.php") ?>
