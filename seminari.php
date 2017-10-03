a5ec6f59e701076ed3d3740641559bd34caeee16
Cristiana Bolchini <cristiana.bolchini@gmail.com>
2017-10-03 17:13:14 +0200

    seminars update

--- seminari.php
+++ seminari.php
@@ -123,13 +123,13 @@
                     <div class='semdate col-lg-10 col-md-10 col-xs-10 clearfix'>
                         <div class='semdetails'><br/><br/>Scarica l'elenco di tutte le attivit&agrave; dell'anno in corso <a href="download.php?fp=stages/seminari201617.pdf"  target=_blank alt="lista attività" class='download'><i class='fa fa-download'></i></a></div>
                     </div>
-                </div> 
-                <div class='seminarwrapper col-lg-12 col-xs-12 col-md-12'>
-                    <div class='semimage col-lg-2 col-md-2 col-xs-6'><a href="download.php?fp=stages/seminariaikido201617.pdf" alt="lista seminari" class='noborder download' target=_blank><img src="stages/aikilistimg.png" /></a></div>
-                    <div class='semdate col-lg-10 col-md-10 col-xs-10 clearfix'>
-                        <div class='semdetails'><br/><br/>Scarica l'elenco dei seminari di aikido dell'anno in corso <a href="download.php?fp=stages/seminariaikido201617.pdf"  target=_blank alt="lista seminari aikido" class='download'><i class='fa fa-download'></i></a></div>
-                    </div>
                 </div--> 
+                <div class='seminarwrapper col-lg-12 col-xs-12 col-md-12'>
+                    <div class='semimage col-lg-2 col-md-2 col-xs-6'><a href="download.php?fp=stages/seminariaikido201718.pdf" alt="lista seminari" class='noborder download' target=_blank><img src="stages/aikilistimg.png" /></a></div>
+                    <div class='semdate col-lg-10 col-md-10 col-xs-10 clearfix'>
+                        <div class='semdetails'><br/><br/>Scarica l'elenco dei seminari di aikido dell'anno in corso <a href="download.php?fp=stages/seminariaikido201718.pdf"  target=_blank alt="lista seminari aikido" class='download'><i class='fa fa-download'></i></a></div>
+                    </div>
+                </div> 
                 <!--div class='clearfix'></div--> 
 
             </div>
